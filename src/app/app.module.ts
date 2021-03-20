9acc1c7699b6c9dda38b48e551b2cfff9a2cd7d8
Bruno Modinuti <modinutidev@gmail.com>
2021-03-20 17:11:20 -0300

    Removido abstração antiga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuestionarioComponent } from './questionario/questionario.component';
-import { QuestaoComponent } from './questionario/questao/questao.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NovotesteComponent } from './novoteste/novoteste.component';
 
 @NgModule({
-  declarations: [AppComponent, QuestionarioComponent, QuestaoComponent, NovotesteComponent],
+  declarations: [AppComponent, NovotesteComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
